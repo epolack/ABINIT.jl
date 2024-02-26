131841fe7b34095a635b24022949c409de1c9fcd
Étienne Polack <87805034+epolack@users.noreply.github.com>
2024-02-26 13:46:43 +0000

    pinning version (#4)

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,11 @@
 using ABINIT
 using Test
 
-@testset begin "version"
-    version = strip(abinit("--version"), '\n')
-    @test version == "9.10.3"
+const PROJECT_ROOT = pkgdir(ABINIT)
+
+@testset "version" begin
+    import TOML
+    pkg_version = TOML.parsefile("$(PROJECT_ROOT)/Project.toml")["compat"]["ABINIT_jll"]
+    version = strip(abinit_cli("--version"), '\n')
+    @test version == pkg_version
 end
